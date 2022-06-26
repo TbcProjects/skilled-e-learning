1d4b93f169abdc7de1c19dbc6354b15debca87e8
TbcProjects <daniel@thebikeclub.co.uk>
2022-06-26 06:55:00 +0100

    add app colors

--- theme/foundations/colors.ts
+++ theme/foundations/colors.ts
@@ -1,47 +1,16 @@
 const colors = {
-  bcDeepPurple: {
-    50: '#F0EAFF',
-    100: '#CEC1F2',
-    200: '#AE9AE6',
-    300: '#8E71DA',
-    400: '#6D49CF',
-    500: '#542FB5',
-    600: '#41258E',
-    700: '#39207C', //This is the default colour
-    800: '#2F1A66',
-    900: '#1C0F3F',
+  primary: {
+    bunting: "#13183F",
+    carnation_pink: "#FFA7C3",
+    scampi: "#666CA3",
+    white: "#FFFFFF",
+    waterloo: "#83869A",
+    violet_red: "#F74780",
   },
-  bcOrange: {
-    50: '#FFF6DA',
-    100: '#FFE4AD',
-    200: '#FFD27D',
-    300: '#FFBF4B',
-    400: '#FFAD1A',
-    500: '#FFA400', //This is the default colour
-    600: '#E69400',
-    700: '#B37300',
-    800: '#815200',
-    900: '#4E3000',
-  },
-  bcPeriwinkle: '#7474C1',
-  bcSienna: '#B51D22',
-  bcOchre: '#B96127',
-  bcCarbon: '#203234',
-  bcLime: '#A5CD39',
-  bcSunflower: '#FFDD2E',
-  bcSky: '#A0DAE3',
-  bcAqua: '#1A95A9',
-  gray: {
-    50: '#F2F2F2',
-    100: '#D9D9D9',
-    200: '#BFBFBF',
-    300: '#A6A6A6',
-    400: '#8C8C8C',
-    500: '#737373',
-    600: '#595959',
-    700: '#404040',
-    800: '#262626',
-    900: '#0D0D0D',
+  gradient: {
+    dodger_blue: "#4851FF",
+    persimmon: "#FF6F48",
+    persian_rose: "#F02AA6",
   },
 };
 
